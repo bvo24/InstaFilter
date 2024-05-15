cfcf5c3e2bcd6ae593e90e2aa7ef499351275b21
bvo24 <brianvo2020@gmail.com>
2024-05-15 12:21:00 -0400

    Feature - Allow users to upload multiple photos

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -5,54 +5,45 @@
 //  Created by Brian Vo on 5/15/24.
 //
 
-import CoreImage
-import CoreImage.CIFilterBuiltins
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image : Image?
+   
+    @State private var pickerItems = [PhotosPickerItem]()
+    @State private var selectedImages = [Image]()
+    
     var body: some View {
         
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
+        VStack{
+            PhotosPicker("Select your picture", selection: $pickerItems,maxSelectionCount: 3, matching: .images)
+            
+            ScrollView{
+                ForEach(0..<selectedImages.count, id: \.self)
+                {
+                    i in
+                    selectedImages[i]
+                        .resizable()
+                        .scaledToFit()
+                    
+                }
+            }
             
         }
-        .onAppear(perform: loadImage)
-    }
+        .onChange(of: pickerItems){
+            Task{
+                selectedImages.removeAll()
+                
+                for item in pickerItems{
+                    if let loadedImage = try await item.loadTransferable(type: Image.self){
+                        selectedImages.append(loadedImage)
+                    }
+                }
+                
+            }
+        }
     
-    //Funcs
-    func loadImage(){
-        let inputImage = UIImage(resource: .example)
-        let beginImage = CIImage(image: inputImage)
-        
-        let context = CIContext()
-        let currentFilter = CIFilter.twirlDistortion()
-        
-        currentFilter.inputImage = beginImage
-        
-        let amount = 1.0
-        let inputKeys = currentFilter.inputKeys
-        
-        if inputKeys.contains(kCIInputIntensityKey){
-            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
-        }
-        if inputKeys.contains(kCIInputRadiusKey){
-            currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)
-        }
-        if inputKeys.contains(kCIInputScaleKey){
-            currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
-        }
-        
-    
-        
-        guard let outputImage = currentFilter.outputImage else{return}
-        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {return}
-        let uiImage = UIImage(cgImage: cgImage)
-        image = Image(uiImage: uiImage)
-        
-        
+            
         
     }
 }
