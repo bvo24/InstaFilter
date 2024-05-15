ec7234cbfc00bd27e2807dad67234673b33727d8
bvo24 <brianvo2020@gmail.com>
2024-05-15 12:01:54 -0400

    Day 63

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -5,17 +5,55 @@
 //  Created by Brian Vo on 5/15/24.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image : Image?
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            image?
+                .resizable()
+                .scaledToFit()
+            
         }
-        .padding()
+        .onAppear(perform: loadImage)
+    }
+    
+    //Funcs
+    func loadImage(){
+        let inputImage = UIImage(resource: .example)
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.twirlDistortion()
+        
+        currentFilter.inputImage = beginImage
+        
+        let amount = 1.0
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey){
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
+        }
+        if inputKeys.contains(kCIInputRadiusKey){
+            currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)
+        }
+        if inputKeys.contains(kCIInputScaleKey){
+            currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
+        }
+        
+    
+        
+        guard let outputImage = currentFilter.outputImage else{return}
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {return}
+        let uiImage = UIImage(cgImage: cgImage)
+        image = Image(uiImage: uiImage)
+        
+        
+        
     }
 }
 
